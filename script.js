d7d32bb078a52b5f3371e1d37242b8ae8591d220
cdalangin <florenzdalangin@gmail.com>
2021-08-01 01:34:04 -0400

    start showing data

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function load(item) {
-    var name = item.name
-    console.log(name)
-    document.getElementById("documentation").setAttribute("href", item.documentation)
-    document.getElementById("desc").innerHTML = item.description
+// In home page, when user clicks on item, it'll store the choice in local storage
+
+function clickHandler(value) {
+    window.location.href = "./results.html"
+    localStorage.setItem('data', JSON.stringify(value));
+}
+
+// In results page, it will load the info through this
+
+function load() {
+    var item = localStorage.getItem('data');
+    console.log(item)
+    JSON.parse(item)
+
+    document.getElementById("documentation").setAttribute("href", JSON.parse(item).documentation)
+    document.getElementById("logo").setAttribute("src", JSON.parse(item).logo)
+
+    document.getElementById("tutoriala").setAttribute("href", JSON.parse(item).tutorialA)
+    document.getElementById("tutorialb").setAttribute("href", JSON.parse(item).tutorialB)
+    document.getElementById("desc").innerHTML = JSON.parse(item).description
+    document.getElementById("choice").innerHTML = JSON.parse(item).name
+    document.getElementById("mainpill").innerHTML = JSON.parse(item).type
+    // localStorage.clear();
 }
--- techData.js
+++ techData.js
@@ -1,18 +1,30 @@
-const express =
-{
-    "name": "Express.js",
-    "logo": "",
-    "description": "Free and open-source web application framework for Node.js. It is used for quicker and smoother designing and building of web applications and APIs. It is specifically designed for building single-page, multi-page, and hybrid web applications.",
-    "documentation": "https://expressjs.com",
-    "tutorialA": "https://youtu.be/L72fhGm1tfE",
-    "tutorialB": "https://youtu.be/JlgKybraoy4"
-}
-
 const node = {
     "name": "Node.js",
-    "logo": "",
+    "type": "Backend",
+    "logo": "https://www.mindrops.com/images/nodejs-image.png",
     "description": "Node.js js an open source platform for the development and execution of JavaScript code server-side. It is useful for the development of applications that need persistent connection from the browser to the server and is often used for real-time applications like chat, news feeds, and web push notifications.",
     "documentation": "https://nodejs.org/en/docs/",
     "tutorialA": "https://youtu.be/ENrzD9HAZK4",
     "tutorialB": "https://youtu.be/TlB_eWDSMt"
+}
+
+const react = {
+    "name": "React.js",
+    "type": "Frontend",
+    "logo": "https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0",
+    "description": "React is a JavaScript library for building user interfaces and modern applications. This allows you to create UIs from small and isolated pieces of code called components.",
+    "documentation": "https://reactjs.org/docs/getting-started.html",
+    "tutorialA": "https://youtu.be/Ke90Tje7VS0",
+    "tutorialB": "https://www.youtube.com/watch?v=4UZrsTqkcW4"
+}
+
+const express =
+{
+    "name": "Express.js",
+    "type": "Backend",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+    "description": "Free and open-source web application framework for Node.js. It is used for quicker and smoother designing and building of web applications and APIs. It is specifically designed for building single-page, multi-page, and hybrid web applications.",
+    "documentation": "https://expressjs.com",
+    "tutorialA": "https://youtu.be/L72fhGm1tfE",
+    "tutorialB": "https://youtu.be/JlgKybraoy4"
 }
