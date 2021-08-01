6ffd5193db4a93e44e8d9aaca217e4c5568f6bc7
cdalangin <florenzdalangin@gmail.com>
2021-07-31 22:28:00 -0400

    start tech data file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function load() {
-    var name = data.name
+function load(item) {
+    var name = item.name
     console.log(name)
+    document.getElementById("documentation").setAttribute("href", item.documentation)
+    document.getElementById("desc").innerHTML = item.description
 }
--- techData.js
+++ techData.js
@@ -1,5 +1,18 @@
-data =
+const express =
 {
-    "name": "Harry",
-    "age": "32"
+    "name": "Express.js",
+    "logo": "",
+    "description": "Free and open-source web application framework for Node.js. It is used for quicker and smoother designing and building of web applications and APIs. It is specifically designed for building single-page, multi-page, and hybrid web applications.",
+    "documentation": "https://expressjs.com",
+    "tutorialA": "https://youtu.be/L72fhGm1tfE",
+    "tutorialB": "https://youtu.be/JlgKybraoy4"
+}
+
+const node = {
+    "name": "Node.js",
+    "logo": "",
+    "description": "Node.js js an open source platform for the development and execution of JavaScript code server-side. It is useful for the development of applications that need persistent connection from the browser to the server and is often used for real-time applications like chat, news feeds, and web push notifications.",
+    "documentation": "https://nodejs.org/en/docs/",
+    "tutorialA": "https://youtu.be/ENrzD9HAZK4",
+    "tutorialB": "https://youtu.be/TlB_eWDSMt"
 }
